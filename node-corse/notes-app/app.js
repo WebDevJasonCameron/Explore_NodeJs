3f1385353caadb7d8e847506840a73a36dfcceb0
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-16 10:49:38 -0500

    found chalk!

--- node-corse/notes-app/app.js
+++ node-corse/notes-app/app.js
@@ -1,13 +1,7 @@
-//
-// Challange: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "your notes..."
-// 3. Export getNotes function
-// 4. From app.js load in and call the function printing message to console
-
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const msg = getNotes();
-
 console.log(msg);
+
+console.log(chalk.green('Success!'));
