694f6e3bc5a46a010a4687bd79560b139383fdd5
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-17 10:15:56 -0500

    learning yargs cmnds

--- node-corse/notes-app/app.js
+++ node-corse/notes-app/app.js
@@ -9,8 +9,15 @@ yargs.version('1.1.0');
 yargs.command({
 	command: 'add',
 	describe: 'Add a new note',
-	handler: function () {
-		console.log('Adding a new note!');
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string',
+		},
+	},
+	handler: function (argv) {
+		console.log('Adding a new note!', argv);
 	},
 });
 
@@ -43,4 +50,4 @@ yargs.command({
 
 // add, remove, read, list
 
-console.log(yargs.argv);
+yargs.parse();
