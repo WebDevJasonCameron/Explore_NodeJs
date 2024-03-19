e98cbfd7859a7da732462e930b5d44c0ab86a63c
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-19 18:40:53 -0500

    understanding writing jsn ad reading jsn with buffer and parse

--- node-corse/playgrounds/1-json.js
+++ node-corse/playgrounds/1-json.js
@@ -1,7 +1,14 @@
-const book = {
-	title: 'Ego is the Enemy',
-	author: 'Ryan Holiday',
-};
+const fs = require('fs');
 
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+// const book = {
+// 	title: 'Ego is the Enemy',
+// 	author: 'Ryan Holiday',
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
