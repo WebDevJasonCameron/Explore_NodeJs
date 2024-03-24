1d169b285651e89427bfe5fffabc1c98590ae127
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-24 08:20:16 -0500

    read notes func added

--- node-corse/notes-app/notes.js
+++ node-corse/notes-app/notes.js
@@ -43,6 +43,18 @@ const listNotes = () => {
 	});
 };
 
+const readNote = (title) => {
+	const notes = loadNotes();
+	const note = notes.find((note) => note.title === title);
+
+	if (note) {
+		console.log(chalk.inverse(note.title));
+		console.log(note.body);
+	} else {
+		console.log(chalk.red.inverse('Note not found'));
+	}
+};
+
 const saveNotes = (notes) => {
 	const dataJSON = JSON.stringify(notes);
 	fs.writeFileSync('notes.json', dataJSON);
@@ -64,4 +76,5 @@ module.exports = {
 	addNote: addNote,
 	removeNote: removeNote,
 	listNotes: listNotes,
+	readNote: readNote,
 };
