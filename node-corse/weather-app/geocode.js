48f555ca96fccfd04bfae40a62df155e904e5f2c
Jason Cameron <dev.jasoncameron@gmail.com>
2024-04-03 05:51:24 -0500

    exploring mapbox api

---
+++ node-corse/weather-app/geocode.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const apis = require('./api.js');
+
+const key = apis.MAPBOX_API;
+
+const geocode = (address, key) => {
+	const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${key}`;
+
+	axios({
+		method: 'get',
+		url: url,
+		responseType: 'json',
+	})
+		.then((res) => {
+			console.log(res);
+		})
+		.catch((err) => {
+			console.log(url);
+			console.log('No map data here... ' + err);
+		});
+};
+
+geocode('huntsville', key);
