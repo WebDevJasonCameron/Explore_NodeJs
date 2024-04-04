07818408c824364a4e72ee92713abeebc2e4e524
Jason Cameron <dev.jasoncameron@gmail.com>
2024-04-03 19:38:44 -0500

    figuring out the async and promise. not there yet

--- node-corse/weather-app/app.js
+++ node-corse/weather-app/app.js
@@ -1,10 +1,12 @@
 const geocodeApp = require('./utils/geocode');
 const forecastApp = require('./utils/forecast');
 
+const coord = [-86.585901, 34.729847];
+
 geocodeApp.geocode('huntsville al', (data) => {
 	console.log(data);
 });
 
-forecastApp.forecast([-75.7088, 44.1545], (data) => {
+forecastApp.forecast([coord[0], coord[1]], (data) => {
 	console.log(data);
 });
--- node-corse/weather-app/utils/forecast.js
+++ node-corse/weather-app/utils/forecast.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const apis = require('../api.js');
 
 const forecast = (coord, callback) => {
-	const lat = coord[0];
-	const long = coord[1];
+	const lat = coord[1];
+	const long = coord[0];
 	const key = apis.WX_API;
 	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
 
